03111d69f0bec1cf2e45c917107a6b1dd7c38349
Jobin George <jobintweets@gmail.com>
2020-05-19 11:03:11 +0530

    delimted file reading implemented

--- src/main/java/com/example/springbatch/ItemReaders/FixedWidthJob.java
+++ src/main/java/com/example/springbatch/ItemReaders/FixedWidthJob.java
@@ -33,7 +33,7 @@ public class FixedWidthJob {
 
   @Bean
   public Step copyFileStep() {
-    return this.stepBuilderFactory.get("copyFileStep")
+    return this.stepBuilderFactory.get("delimited_file")
       .<Customer, Customer>chunk(10)
       .reader(customerItemReader(null))
       .writer(itemWriter())
@@ -42,33 +42,52 @@ public class FixedWidthJob {
 
   @Bean
   public Job job() {
-    return this.jobBuilderFactory.get("job")
+    return this.jobBuilderFactory.get("delimited_job")
       .start(copyFileStep())
       .build();
   }
 
 
-  @Bean
-  @StepScope
-  public FlatFileItemReader<Customer> customerItemReader(
-    @Value("#{jobParameters['customerFile']}") Resource inputFile) {
-    return new FlatFileItemReaderBuilder<Customer>()
-      .name("customerItemReader")
-      .resource(inputFile)
-      .fixedLength()
-      .columns(new Range[]{new Range(1, 11),
-        new Range(12, 12), new Range(13, 22),
-        new Range(23, 26), new Range(27, 46),
-        new Range(47, 62), new Range(63, 64),
-        new Range(65, 69)})
-      .names(new String[]{"firstName",
-        "middleInitial", "lastName",
-        "addressNumber", "street",
-        "city", "state",
-        "zipCode"})
-      .targetType(Customer.class)
-      .build();
-  }
+//  @Bean
+//  @StepScope
+//  public FlatFileItemReader<Customer> customerItemReader(
+//    @Value("#{jobParameters['customerFile']}") Resource inputFile) {
+//    return new FlatFileItemReaderBuilder<Customer>()
+//      .name("customerItemReader")
+//      .resource(inputFile)
+//      .fixedLength()
+//      .columns(new Range[]{new Range(1, 11),
+//        new Range(12, 12), new Range(13, 22),
+//        new Range(23, 26), new Range(27, 46),
+//        new Range(47, 62), new Range(63, 64),
+//        new Range(65, 69)})
+//      .names(new String[]{"firstName",
+//        "middleInitial", "lastName",
+//        "addressNumber", "street",
+//        "city", "state",
+//        "zipCode"})
+//      .targetType(Customer.class)
+//      .build();
+//  }
+@Bean
+@StepScope
+public FlatFileItemReader<Customer> customerItemReader(
+  @Value("#{jobParameters['customerFile']}") Resource inputFile) {
+  return new FlatFileItemReaderBuilder<Customer>()
+    .name("customerItemReader")
+    .resource(inputFile)
+    .delimited()
+    .names(new String[]{"firstName",
+      "middleInitial",
+      "lastName",
+      "addressNumber",
+      "street",
+      "city",
+      "state",
+      "zipCode"})
+    .targetType(Customer.class)
+    .build();
+}
 
   @Bean
   public ItemWriter<Customer> itemWriter() {
@@ -77,7 +96,8 @@ public class FixedWidthJob {
 
 
   public static void main(String[] args) {
-    List<String> realArgs = Arrays.asList("customerFile=file:/Users/jobingeorge/Desktop/spring-batch/src/main/resources/input/customerFixedWidth.txt");
+//    List<String> realArgs = Arrays.asList("customerFile=file:/Users/jobingeorge/Desktop/spring-batch/src/main/resources/input/customerFixedWidth.txt");
+    List<String> realArgs = Arrays.asList("customerFile=file:/Users/jobingeorge/Desktop/spring-batch/src/main/resources/input/customers.txt");
 
     SpringApplication.run(FixedWidthJob.class, realArgs.toArray(new String[1]));
   }
--- src/main/java/com/example/springbatch/ItemReaders/itemreaders.md
+++ src/main/java/com/example/springbatch/ItemReaders/itemreaders.md
@@ -1,3 +1,5 @@
 **LineMapper** - This class will take each line of a file as a String and convert it into a domain object (item) to be processed.
 
+The record will first be tokenized by the LineTokenizer into a FieldSet. From there, the FieldSet will be mapped into your domain object by the FieldSetMapper
+
 **DefaultLineMapper** - parsing the line into a FieldSet and then mapping the fields of the FieldSet to a domain object
